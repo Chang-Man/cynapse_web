27b1ae5610052c5b7ad88cbf39cbf0eba16c3fe7
ChrisLee02 <hdchris765@snu.ac.kr>
2022-07-29 17:39:20 +0900

    "page1 complete"

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import './App.css';
 import ReactPageScroller from "react-page-scroller";
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
-
+import {createTheme, ThemeProvider} from "@mui/material";
 
 function App() {
     return (
-        <div className={"wrapper"}>
-            <ReactPageScroller >
-                <Page1></Page1>
-                <Page2></Page2>
-            </ReactPageScroller>
-        </div>
+
+        <ThemeProvider theme={THEME}>
+            <div className={"wrapper"}>
+                <header className={"header"}>dd</header>
+                <ReactPageScroller >
+                    <Page1></Page1>
+                    <Page2></Page2>
+                    <Page1></Page1>
+                </ReactPageScroller>
+            </div>
+        </ThemeProvider>
+
 
     );
 }
+
+const THEME = createTheme({
+    typography: {
+        "fontFamily": `-apple-system, BlinkMacSystemFont, 'NotoSans'`,
+        "fontSize": 14,
+        "fontWeightLight": 500,
+        "fontWeightRegular": 600,
+        "fontWeightMedium": 700
+    }
+});
 
 export default App;
