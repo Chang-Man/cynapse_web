d10d237b73a2f1c70ac94f3589b9d0b3d9dad465
ChrisLee02 <hdchris765@snu.ac.kr>
2022-07-29 15:30:34 +0900

    "."

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Profile.module.scss';
+import styles from '../../styles/Profile.module.scss';
 import {Avatar, Card, CardContent, Typography} from "@mui/material";
 
 
