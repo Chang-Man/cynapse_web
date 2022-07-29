cd56cdd28a25dc7082a393f1aea3fb26ce5e282e
ChrisLee02 <hdchris765@snu.ac.kr>
2022-07-29 14:39:39 +0900

    "React-Page-Scroller"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Profile from "./Profile";
 import ChrisLee from "./Image/ChrisLee.jpg";
+import ReactPageScroller from "react-page-scroller";
+
 
 function App() {
     return (
-        <div>
+        <ReactPageScroller>
             <Profile role={"Developer"} belong={"서울대학교 컴퓨터공학부"} contact={"hdchris765@snu.ac.kr"} imgSrc={ChrisLee}
                      name={"이하동"}></Profile>
-        </div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </ReactPageScroller>
     );
 }
 
