7f7768ae1aeca9d51613e95759d3e72d9d57f5ec
Chang-Man <changagim@snu.ac.kr>
2022-08-01 10:11:53 +0900

    add move-to-top button

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import ReactPageScroller from 'react-page-scroller';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
-import { ThemeProvider, createTheme } from '@mui/material';
+import { ThemeProvider, createTheme, IconButton } from '@mui/material';
 import Page4 from './components/Page4';
 import Logo from './public/images/Cynapse_Logo_white.png';
+import { useEffect, useState } from 'react';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    console.log(currentPage);
+  }, [currentPage, setCurrentPage]);
   return (
     <ThemeProvider theme={THEME}>
       <div className={styles.wrapper}>
         <header className={styles.header}>
           <img src={Logo} alt={'logo'} className={styles.logo} />
         </header>
-        <ReactPageScroller>
+        <IconButton
+          className={styles.upButton}
+          onClick={() => {
+            setCurrentPage(0);
+          }}
+        >
+          <ArrowUpwardIcon fontSize='large' />
+        </IconButton>
+        <ReactPageScroller
+          pageOnChange={(num) => {
+            setCurrentPage(num);
+          }}
+          customPageNumber={currentPage}
+        >
           <Page1></Page1>
           <Page2></Page2>
           <Page3></Page3>
