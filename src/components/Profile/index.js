5307cfefef235bcc270024dcdda518e04c2b7697
ChrisLee02 <hdchris765@snu.ac.kr>
2022-08-05 17:17:47 +0900

    "Refact/글씨크기"

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,51 +1,58 @@
 import React from 'react';
-import { Avatar, Typography } from '@mui/material';
-import { Box } from '@mui/system';
+import {Avatar, Typography} from '@mui/material';
+import {Box} from '@mui/system';
 
-const Profile = ({ name, imgSrc, role, profile, belong }) => {
-  return (
-    <Box sx={{ width: 190 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Avatar
-          alt={name}
-          src={imgSrc}
-          sx={{backgroundColor:'white', width: 120, height: 120, margin: 0 }}
-        />
-      </Box>
-      <Typography align={'center'} variant='h6'>
-        {name}
-      </Typography>
-      <Box
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-      >
-        <Typography fontSize={14} variant={'body1'} width={'fit-content'}>
-          {belong}
-        </Typography>
-        <Typography>{role}</Typography>
-        {profile ? (
-          <a
-            target={'_blank'}
-            style={{
-              fontFamily: 'NotoSans',
-              fontSize: 14,
-              fontWeight: 800,
-              color: 'white',
-            }}
-            rel='noreferrer'
-            href={`https://${profile}`}
-          >
-            {profile}
-          </a>
-        ) : (
-          'temp'
-        )}
+const Profile = ({name, imgSrc, role, profile, belong}) => {
+    return (
+        <Box sx={{width: 190}}>
+            <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                <Avatar
+                    alt={name}
+                    src={imgSrc}
+                    sx={{backgroundColor: 'white', width: 120, height: 120, margin: 0}}
+                />
+            </Box>
+            <Box fontSize={15} sx={{display: 'flex', justifyContent: 'center'}}>
+                {name}
+            </Box>
+            <Box
+                sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}
+            >
+                <Box fontSize={12} sx={{display: 'flex', justifyContent: 'center'}}>
+                    {belong}
+                </Box>
+                <Box fontSize={14} sx={{display: 'flex', justifyContent: 'center'}}>
+                    {role}
+                </Box>
 
-        {/*<Typography color={"text.secondary"}>
+                {profile ? (
+                    <a
+
+                        target={'_blank'}
+                        style={{
+                            fontFamily: 'NotoSans',
+                            textDecoration: 'none',
+                            fontSize: 12,
+                            fontWeight: 600,
+                            color: 'white',
+                        }}
+                        rel='noreferrer'
+                        href={`https://${profile}`}
+                    >
+                        {profile}
+                    </a>
+                ) : (
+                    <Box fontSize={12} sx={{display: 'flex', justifyContent: 'center'}}>
+                        {'tmp'}
+                    </Box>
+                )}
+
+                {/*<Typography color={"text.secondary"}>
 
                 </Typography>*/}
-      </Box>
-    </Box>
-  );
+            </Box>
+        </Box>
+    );
 };
 
 export default Profile;
