f6f6fab079450b8307f5a04e69895da22e9a7b3f
ChrisLee02 <hdchris765@snu.ac.kr>
2022-08-12 16:51:59 +0900

    "구글 애널리틱스를 위한 수정"

--- src/components/Page1/index.js
+++ src/components/Page1/index.js
@@ -2,34 +2,34 @@ import React from 'react';
 import styles from '../../styles/Page1.module.scss';
 import Logo from '../../public/images/Cynapse_Logo_white_only.png';
 
-import { Avatar, Box, Divider, Typography } from '@mui/material';
+import {Box, Typography} from '@mui/material';
 
 const Page1 = () => {
-  return (
-    <div className={styles.wrapper}>
-      <Box
-        display={'flex'}
-        flexDirection={'column'}
-        alignItems={'center'}
-        sx={{ width: 300, mb: 2 }}
-      >
-        <div>
-          <Typography variant={'h3'} align='left'>
-            Cynapse
-          </Typography>
-        </div>
-        <br />
-        <div>
-          <Typography align='center'>능력, 브랜드가 되다</Typography>
-          <Typography marginBottom={'10%'} color={'#FF5126'} align='center'>
-            커뮤니티형 작업 공간
-          </Typography>
-        </div>
-      </Box>
-      <img style={{marginTop:"40px"}} className={styles.mainImg} src={Logo} alt={'mainImg'} />
-      <span>다양한 사람들과 협업해</span>
-      <span>하나의 브랜드를 만들어보세요.</span>
-      {/*<Box sx={{width: 320, height: 300}} justifyContent="left">
+    return (
+      <div className={styles.wrapper}>
+          <Box
+            display={'flex'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            sx={{width: 300, mb: 2}}
+          >
+              <div>
+                  <Typography variant={'h3'} align='left'>
+                      Cynapse
+                  </Typography>
+              </div>
+              <br/>
+              <div>
+                  <Typography align='center'>능력, 브랜드가 되다</Typography>
+                  <Typography marginBottom={'10%'} color={'#FF5126'} align='center'>
+                      커뮤니티형 작업 공간
+                  </Typography>
+              </div>
+          </Box>
+          <img style={{marginTop: "40px"}} className={styles.mainImg} src={Logo} alt={'mainImg'}/>
+          <span>다양한 사람들과 협업해</span>
+          <span>하나의 브랜드를 만들어보세요.</span>
+          {/*<Box sx={{width: 320, height: 300}} justifyContent="left">
                 <div className={styles.roleBox}>
                     <div className={styles.justifyCenter} style={{width: 70}}>
 
@@ -50,8 +50,8 @@ const Page1 = () => {
                 <br/>
                 <Typography align={'center'}>하나의 브랜드를 만들어보세요.</Typography>
             </Box>*/}
-    </div>
-  );
+      </div>
+    );
 };
 
 export default Page1;
